a8d60bda7a734247f616dac3d2f9fcccfd588c5d
木泠 <CleMooling@outlook.com>
2025-12-10 20:40:09 +0800

    feat: v0.1.0 demo version

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use daemon_console_lite::TerminalApp;
+use daemon_console_lite::logger::LogLevel::Info;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut app = TerminalApp::new();
-    // app.enable_raw_mode_on_windows();
-    app.init_terminal("Starting MCManagerFramework.").await?;
+    let _ = app.enable_raw_mode_on_windows();  // Add this to make sure console works fine in Windows.
+    app.app_name = "MCManagerFramework".to_string();
+    app.init_terminal("Starting MCManagerFramework...").await?;
+    handle_tab_completion(&mut app);
     app.info("Framework features are not implemented yet.");
-    app.warn(
-        "Due to upstream issue, Ctrl+C features work wrongly. Suggest using alias `exit` to exit.",
-    );
     while let Some(input) = app.read_input().await? {
         if handle_input(&mut app, &input) {
             break;
@@ -18,8 +18,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
+fn handle_tab_completion(app: &mut TerminalApp) {
+    app.print_log_entry("Registering tab completions...");
+    app.enable_tab_completion();
+    app.register_tab_completions_with_desc("", &[("mcm", "Main command.")]);
+}
+
 fn handle_input(app: &mut TerminalApp, input: &str) -> bool {
     match input.trim().to_lowercase().as_str() {
+        "mcm" => {
+            let app_name = app.app_name.clone();
+            app.logger(Info, "This is the main command.\nVersion: 0.1.0", Some(&app_name));
+            false
+        }
         "exit" => {
             app.info("Exiting MCManagerFramework.");
             true
